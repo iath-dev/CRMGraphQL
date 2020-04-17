b37ad65706f4548f5fe167a6367c66f53bab5837
iath-dev <developer.iath@gmail.com>
2020-04-16 23:19:29 -0500

    Eslint Añadido

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { ApolloServer, gql } = require('apollo-server')
 
 // Servidor
-const server = new ApolloServer();
+const server = new ApolloServer()
 
 // Arrancar el Servidor
 server.listen().then(({ url }) => {
-    console.log(`Servidor corriendo en ${url}`)
+  console.log(`Servidor corriendo en ${url}`)
 })
