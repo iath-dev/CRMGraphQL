ca52e342af11917f327ae00dde32c1f1929c0e9a
iath-dev <developer.iath@gmail.com>
2020-04-16 23:13:55 -0500

    7. Creando un servidor de Apollo

---
+++ index.js
@@ -0,0 +1,9 @@
+const { ApolloServer, gql } = require('apollo-server')
+
+// Servidor
+const server = new ApolloServer();
+
+// Arrancar el Servidor
+server.listen().then(({ url }) => {
+    console.log(`Servidor corriendo en ${url}`)
+})
