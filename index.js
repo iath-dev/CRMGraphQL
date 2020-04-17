d792af0ca15c7c16195e5afe31fedcc3cefa09e9
iath-dev <developer.iath@gmail.com>
2020-04-16 23:31:09 -0500

    12. Agregando Types y Resolvers en el Codigo

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 const { ApolloServer, gql } = require('apollo-server')
 
+// Schema
+const typeDefs = gql`
+    type Query {
+        getCourses: [Course]
+    }
+
+    type Course {
+        titulo: String,
+        tecnologia: String
+    }
+`
+
+const cursos = [
+  {
+    titulo: 'JavaScript Moderno Guía Definitiva Construye +10 Proyectos',
+    tecnologia: 'JavaScript ES6'
+  },
+  {
+    titulo: 'React – La Guía Completa: Hooks Context Redux MERN +15 Apps',
+    tecnologia: 'React'
+  },
+  {
+    titulo: 'Node.js – Bootcamp Desarrollo Web inc. MVC y REST API’s',
+    tecnologia: 'Node.js'
+  },
+  {
+    titulo: 'ReactJS Avanzado – FullStack React GraphQL y Apollo',
+    tecnologia: 'React'
+  }
+]
+
+// Resolvers
+const resolvers = {
+  Query: {
+    getCourses: () => cursos
+  }
+}
+
 // Servidor
-const server = new ApolloServer()
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
 // Arrancar el Servidor
 server.listen().then(({ url }) => {
